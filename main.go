2806742924dac6c7580489dee4f4b93189a07b97
Dolph Mathews <dolph.mathews@gmail.com>
2015-05-29 14:40:35 +0000

    do not build custom cert pool

--- main.go
+++ main.go
@@ -1,30 +1,15 @@
 package main
 
-import "crypto/tls"
 import "fmt"
 import "io/ioutil"
 import "net/http"
 import "os"
 import "strings"
 
-import "github.com/certifi/gocertifi"
-
 
 func main() {
-    cert_pool, err := gocertifi.CACerts()
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-
-    transport := &http.Transport{
-        TLSClientConfig: &tls.Config{RootCAs: cert_pool},
-    }
-    client := &http.Client{
-        Transport: transport,
-    }
-
-    resp, err := client.Get("https://icanhazip.com/")
+    // this requires Go 1.4 due to the TLS implementation
+    resp, err := http.Get("https://icanhazip.com/")
     if err != nil {
         fmt.Println(err)
         os.Exit(1)
