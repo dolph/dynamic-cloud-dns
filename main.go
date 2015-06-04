7154050783f28e6a57deaea7304e214229b71a2e
Dolph Mathews <dolph.mathews@gmail.com>
2015-06-04 20:57:35 +0000

    auth with rax

--- main.go
+++ main.go
@@ -1,30 +1,33 @@
 package main
 
 import (
+    "bytes"
+    "flag"
     "fmt"
     "io/ioutil"
+    "encoding/json"
     "net/http"
     "os"
     "strings"
 )
 
 
+func fatalError(err error, message string) {
+    if err != nil {
+        fmt.Println(message)
+        fmt.Println(err)
+        panic(err)
+    }
+}
+
 func icanhazip() string {
     // this requires Go 1.4 due to the TLS implementation
     resp, err := http.Get("https://icanhazip.com/")
-    if err != nil {
-        fmt.Println("Unable to fetch icanhazip.")
-        fmt.Println(err)
-        os.Exit(1)
-    }
+    fatalError(err, "Unable to fetch icanhazip.")
 
     defer resp.Body.Close()
     body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        fmt.Println("Unable to read response body.")
-        fmt.Println(err)
-        os.Exit(1)
-    }
+    fatalError(err, "Unable to read response body from icanhazip.")
 
     // icanhazip does nothing but return us an IP
     ip := string(body)
@@ -35,9 +38,58 @@ func icanhazip() string {
     return ip
 }
 
+func authenticate(username string, apiKey string) string {
+    client := &http.Client{}
+
+    body := fmt.Sprintf(
+        `{
+            "auth": {
+                "RAX-KSKEY:apiKeyCredentials": {
+                    "username": "%s",
+                    "apiKey": "%s"
+                }
+            }
+        }`,
+        username, apiKey)
+    req, err := http.NewRequest(
+        "POST",
+        "https://identity.api.rackspacecloud.com/v2.0/tokens",
+        bytes.NewBuffer([]byte(body)))
+    req.Header.Set("Content-Type", "application/json")
+    fatalError(err, "Unable to build authentication request.")
+    resp, err := client.Do(req)
+    fmt.Println("HTTP", resp.StatusCode)
+    if resp.StatusCode == 401 {
+        fatalError(err, "Unable to authenticate with Rackspace.")
+    }
+
+    text, err := ioutil.ReadAll(resp.Body)
+    fatalError(err, "Unable to read authentication response.")
+
+    var data map[string]interface{}
+    if err := json.Unmarshal(text, &data); err != nil {
+        panic(err)
+    }
+    access := data["access"]
+    token := access.(map[string]interface{})["token"]
+    return token.(map[string]interface{})["id"].(string)
+}
 
 func main() {
     ip := icanhazip()
 
     fmt.Println(ip)
+
+    usernamePtr := flag.String("username", "", "Rackspace account username")
+    apiKeyPtr := flag.String("api-key", "", "Rackspace API key")
+
+    flag.Parse()
+
+    if *usernamePtr == "" || *apiKeyPtr == "" {
+        fmt.Println("Rackspace -username and -api-key required.")
+        fmt.Println("See -help for more info.")
+        os.Exit(1)
+    }
+
+    token := authenticate(*usernamePtr, *apiKeyPtr)
 }
